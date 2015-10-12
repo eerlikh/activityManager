f584493289e9544a1b7038dbf66b5613ad363aa3
Eduard Erlikh <eerlikh7@gmail.com>
2015-10-12 17:48:47 -0400

    make it better

--- client/js/app.js
+++ client/js/app.js
@@ -8,12 +8,27 @@ angular.module('ActivityManager')
     $scope.activities = [];
     $scope.newActivity = {};
 
-    $http.get('/api/activities').then(function(response){
-      $scope.activities = response.date;
-    })
+    $scope.getActivity = function(){
+      $http.get('/api/activities').then(function(response){
+        $scope.activities = response.data;
+      });
+    };
 
     $scope.createActivity = function(){
-
+      console.log('inside createActivity');
+      $http.post('/api/activities', $scope.newActivity).then(function(response){
+        console.log('activ', $scope.activities);
+        $scope.activities.push(response.data);
+      });
     };
+
+    $scope.removeActivity = function(activity){
+      var url = '/api/activities/' + activity._id;
+      $http.delete(url).then(function(response){
+        $scope.getActivity();
+      });
+    };
+
+    $scope.getActivity();
 
   }]);
--- server.js
+++ server.js
@@ -25,6 +25,20 @@ app.get('/api/activities', function(req, res){
   });
 });
 
+app.post('/api/activities', function(req, res){
+  var activity = new Activity(req.body);
+  activity.save(function(err, activity){
+    res.json(activity);
+  });
+});
+
+app.delete('/api/activities/:id', function(req, res){
+  var id = req.params.id;
+  Activity.findByIdAndRemove(id, function(err,activity){
+    res.json({status: 202, message: 'Success'});
+  });
+});
+
 //start the app
 app.listen('8080', function(){
   console.log('...listening to you');
